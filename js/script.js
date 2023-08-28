b567bd0c45def4aa5572e2f1fb967fac7cc1d4d3
matheus-sousa-nascimento <143366856+matheus-sousa-nascimento@users.noreply.github.com>
2023-08-28 07:16:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,34 @@ hamburguer.addEventListener("click", () => {
 });
 
 // pg characters
-const marvelApiKey = '00bd7dc3ebd4da7509c41d3af9f8691c';
+let marvelApiKey = '00bd7dc3ebd4da7509c41d3af9f8691c';
+let ts = "1693216557450";
+let hashVal = "d527f316d361aa621c33f9d1e23adc36";
 
-function getAllHeroes() {
-    fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=00bd7dc3ebd4da7509c41d3af9f8691c`)
+let input = document.getElementById("input-box");
+let button = document.getElementById("submit-button");
+let showContainer = document.getElementById(".list");
+
+let date = new Date();
+console.log(date.getTime());
+
+const [timestamp, ApiKey, hash] = [ts, marvelApiKey, hashVal]
+
+button.addEventListener.addEventListener("click", (getRsult = async () =>{
+    if (input.value.trim().lenght < 1){
+        alert("input cannot be blank");
+    }
+    showContainer.innerHTML = "";
+    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&name=${input.value}`
+
+    const response = await fetch(url);
+    constjsonData = await response.json();
+    json.Data["results"].forEach((element) => {
+        showContainer.innerHTML = `<div class="container-character-image"> <img src="${element.thumbnail["path"] + "." + element.description}</div>"
+        </div>;`
+    });
+}));
+
+window.onload = () => {
+    getRsult()
 }
